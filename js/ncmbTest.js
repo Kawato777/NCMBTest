e18a1bca2ea639150f316d8ec0c7c413bc4e579d
Kawato <keiaru0512@gmail.com>
2021-06-28 21:50:56 +0900

    ファイルストア実装

--- js/ncmbTest.js
+++ js/ncmbTest.js
@@ -1,15 +1,18 @@
 var ncmb = new NCMB("8e043f4706a26a64b25e025a19d84abd9440421387079b3e31946a8a69e970fd","fe6b384c00b431aa428a4ed68241b527ecc5e241c573d66ea05cf79c7e370d08");
 /* コード入力↓↓↓ */
-// クラスのTestClassを作成
-var TestClass = ncmb.DataStore("TestClass");
 
-// データストアへの登録
-var testClass = new TestClass();
-testClass.set("message", "Hello, NCMB!");
-testClass.save()
-            .then(function(){
-                // 保存に成功した場合の処理
-            })
-            .catch(function(err)){
-                // 保存に失敗した場合の処理
-            }
+// ファイルストア
+var onFormSend = function(){
+    var fileName = document.getElementById("file-name").value;
+    var fileData = document.getElementById("file-data").files[0];
+
+    ncmb.File
+        .upload(fileName, fileData).then(function(res){
+            // アップロード後処理
+            document.getElementById("result").innerHTML = "OK!";
+        })
+        .catch(function(err){
+            // エラー処理
+            document.getElementById("result").innerHTML = err;
+        });
+}
